2fb87ea1b6a7bedbb3560f112572d98e3737aa78
Wayne Crouch <wgcrouch@gmail.com>
2010-08-16 13:50:45 +0100

    Added index and js files

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>jsSnake</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script src="starfield.js"></script>
+        <script>
+
+			var context;
+			
+			$(document).ready(function() {
+
+				context = document.getElementById('field').getContext("2d");
+							
+				starfield = new Starfield({
+					context : context
+				});
+
+				starfield.clear();
+
+				interval = setInterval(starfield.update,10);
+
+
+			});
+
+		</script>
+    </head>
+    <body>
+        <canvas id="field" width="400px" height="400px"></canvas>
+
+    </body>
+</html>
---
+++ starfield.js
@@ -0,0 +1,73 @@
+
+
+var Starfield = function(options) {
+
+	var starfield = {};
+
+	//Merge default settings with settings that have been passed in
+	var settings = jQuery.extend({
+		context: null,  //Canvas context
+		height : 400,   //Height of the context
+		width  : 400,   //Width of the context
+		focal_length : 50,
+		max_z : 600,
+		star_count : 40,
+		speed : 5,
+		star_width : 8
+	},
+	options);
+	
+	var origin = {};
+	origin.x = Math.floor(settings.width / 2);
+	origin.y = Math.floor(settings.height / 2);
+
+	var stars = [];
+
+	for (var i = 0; i < settings.star_count; i++) {
+		stars[i] = {};
+		stars[i].x = -origin.x + Math.floor(Math.random() * settings.width);
+		stars[i].y = -origin.y + Math.floor(Math.random() * settings.height),
+		stars[i].z = Math.floor(Math.random() * settings.max_z);
+	}
+
+
+
+	starfield.draw = function()
+	{
+		context.fillStyle = '#FFF';
+		for (var i = 0; i < settings.star_count; i++) {
+			star = stars[i];
+			var scale_ratio = settings.focal_length / (settings.focal_length + star.z);
+			var x = origin.x + star.x * scale_ratio;
+			var y = origin.y + star.y * scale_ratio;
+			var w  = settings.star_width * scale_ratio;
+			var h = w;			
+			context.fillRect(x, y, w, h);
+		}
+	}
+
+	starfield.update = function()
+	{
+		starfield.clear();
+		for (var i = 0; i < settings.star_count; i++) {
+			stars[i].z += settings.speed;
+			if (stars[i].z >= settings.max_z) {
+				stars[i].z = 0;
+			}
+		}
+		starfield.draw();
+	}
+
+	starfield.clear = function() {
+		context.fillStyle = '#000';
+		context.fillRect(0,0, settings.width, settings.height);
+	};
+	return starfield;
+}
+
+var Star = new function() {
+
+	var star = {};
+	
+	return star;
+}
